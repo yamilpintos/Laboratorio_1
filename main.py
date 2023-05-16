5bfafc4914f8fc3161bdb828c9c8613455ebc2fd
yamilpintos <yamilpintods18@gmail.com>
2023-05-16 14:47:19 -0300

    vamoquesi

--- main.py
+++ main.py
@@ -25,13 +25,15 @@ df = pd.read_csv("movies_dataset_final1.csv", parse_dates=["release_date"])
 
 @app.get("/peliculas_mes")
 async def peliculas_mes(mes: str):
+    meses: Dict[str, int] = {'enero': 1, 'febrero': 2, 'marzo': 3, 'abril': 4, 'mayo': 5, 'junio': 6,
+                             'julio': 7, 'agosto': 8, 'septiembre': 9, 'octubre': 10, 'noviembre': 11, 'diciembre': 12}
     
-    datetime_obj = datetime.strptime(mes, "%m") #Convertimos a datatime 
+    datetime_objeto = datetime.strptime(mes, "%m") #Convertimos a datatime 
     
-    filtered_movies = df[df["release_date"].dt.month == datetime_obj.month]#filtramos peliculas de ese mes 
+    filtered_movies = df[df["release_date"].dt.month == datetime_objeto.month]#filtramos peliculas de ese mes 
     
     cantidad = len(filtered_movies)  # Contamos la cantidad de películas
-    return {"mes": datetime_obj.strftime("%B"), "cantidad": cantidad}
+    return {"mes": datetime_objeto.strftime("%B"), "cantidad": cantidad}
 
 
 @app.get("/peliculas_dia")
@@ -39,9 +41,9 @@ async def peliculas_dia(dia: int):
     
     dia_semana = calendar.day_name[dia] # Convertir el número de día a un nombre de día de la semana
 
-    filtered_movies = df[df["release_date"].dt.day_name() == dia_semana]#filtramos peliculas por dia de estreno
+    filtro_movies = df[df["release_date"].dt.day_name() == dia_semana]#filtramos peliculas por dia de estreno
    
-    cantidad = len(filtered_movies)  # Contamos la cantidad de películas
+    cantidad = len(filtro_movies)  # Contamos la cantidad de películas
     
     return {"dia": dia_semana, "cantidad": cantidad}
 
@@ -102,33 +104,33 @@ async def franquicia(franquicia):
 
     ganancia_promedio = ganancia_total / cantidad
 
-    response_data = {"franquicia": franquicia, "cantidad": cantidad,
+    respuesta_data = {"franquicia": franquicia, "cantidad": cantidad,
                      "ganancia_total": ganancia_total, "ganancia_promedio": ganancia_promedio}
-    # Convertir el objeto JSON a una cadena compatible con JSON
-    response_str = json.dumps(response_data)
+    
+    response_str = json.dumps(respuesta_data) # Convertir el objeto JSON a una cadena compatible con JSON
 
     return response_str
 
-movies_subset = df[['production_companies_name', 'title', 'genre_name']]
+movies_1 = df[['production_companies_name', 'title', 'genre_name']]
 
-movies_subset = movies_subset.dropna()
-
-
-movies_subset = movies_subset.head(5000)
+movies_1 = movies_1.dropna()
 
 
-movies_subset = pd.get_dummies(movies_subset, columns=[
+movies_1 = movies_1.head(5000)
+
+
+movies_1 = pd.get_dummies(movies_1, columns=[
                                'production_companies_name', 'genre_name'])
 
 
 model = NearestNeighbors(n_neighbors=6, metric='cosine', algorithm='brute')
-# Entrenar modelo de vecinos cercanos
-model.fit(movies_subset.drop('title', axis=1))
+
+model.fit(movies_1.drop('title', axis=1))# Entrenar modelo de vecinos cercanos
 
 
 scaler = StandardScaler()
 movies_norm = scaler.fit_transform(
-    movies_subset.drop('title', axis=1))  # Normalizar los datos
+    movies_1.drop('title', axis=1))  # Normalizar los datos
 
 
 joblib.dump(model, 'model.joblib')
@@ -143,13 +145,13 @@ async def recomendacion(titulo: str):
     genero = df[df['title'] == titulo]['genre_name'].values[0]
 
     # Filtrar películas por género
-    genre_movies = movies_subset[movies_subset['genre_name'] == genero]
+    genre_movies = movies_1[movies_1['genre_name'] == genero]
 
-    title_features = genre_movies[genre_movies['title'] == titulo].drop(
+    title_caracteristica = genre_movies[genre_movies['title'] == titulo].drop(
         'title', axis=1)
-    title_features = scaler.transform(title_features)
+    title_caracteristica = scaler.transform(title_caracteristica)
 
-    distances, indices = model.kneighbors(title_features, n_neighbors=6)
+    distances, indices = model.kneighbors(title_caracteristica, n_neighbors=6)
 
     titles = []
     for i in range(1, len(distances.flatten())):
@@ -157,3 +159,4 @@ async def recomendacion(titulo: str):
             df[df.index == indices.flatten()[i]]['title'].values[0])
 
     return {'lista recomendada': titles}
+
